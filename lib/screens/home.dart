5b22e2e8a439573f3e4f0ac063603cd0bd57ae0c
Emmanuel <emmanthomp3@gmail.com>
2026-01-04 20:08:44 -0600

    switching content

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery.dart';
+import 'package:shopping_list_app/screens/new_item.dart';
 import 'package:shopping_list_app/widgets/dummy_item.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
+    );
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget content = const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Let\'s go get some GROCERIES!!!!',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            textAlign: TextAlign.center,
+          ),
+        ])
+      ],
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => GroceryListWidget(
+          index: index,
+          groceryItems: _groceryItems,
+        ),
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Your Groceries'),
+          actions: [
+            IconButton(onPressed: _addItem, icon: const Icon(Icons.add))
+          ],
         ),
-        body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (context, index) => GroceryListWidget(index: index),
-        ));
+        body: content);
   }
 }
--- lib/widgets/dummy_item.dart
+++ lib/widgets/dummy_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery.dart';
 
 class GroceryListWidget extends StatelessWidget {
-  const GroceryListWidget({super.key, required this.index});
+  const GroceryListWidget(
+      {super.key, required this.index, required this.groceryItems});
 
   final int index;
+  final List<GroceryItem> groceryItems;
 
   @override
   Widget build(BuildContext context) {
