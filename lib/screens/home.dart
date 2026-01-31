1f17287d1e699923ed339c11b3122723a67054e2
Emmanuel <emmanthomp3@gmail.com>
2026-01-31 14:22:42 -0600

    yo this is an example of future builder dont use this

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery.dart';
 import 'package:shopping_list_app/screens/new_item.dart';
 import 'package:shopping_list_app/widgets/dummy_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shopping_list_app/data/categories.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,7 +15,89 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  //var _isLoading = true;
+  late Future<List<GroceryItem>> _loadedItems;
+  String? _error;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //_loadItems();
+    _loadedItems = _loadItems();
+  }
+
+  Future<List<GroceryItem>> _loadItems() async {
+    final url = Uri.https('shopping-list-7ec0f-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    //try {
+    final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      throw Exception('Failed to fetch grocery please try again.');
+//         setState(() {
+//           _error = '''
+// Something has gone terribly wrong. Please, please try again later.
+
+//     ⡴⠒⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠉⠳⡆⠀
+//     ⣇⠰⠉⢙⡄⠀⠀⣴⠖⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠁⠙⡆
+//     ⠘⡇⢠⠞⠉⠙⣾⠃⢀⡼⠀⠀⠀⠀⠀⠀⠀⢀⣼⡀⠄⢷⣄⣀⠀⠀⠀⠀⠀⠀⠀⠰⠒⠲⡄⠀⣏⣆⣀⡍
+//     ⠀⢠⡏⠀⡤⠒⠃⠀⡜⠀⠀⠀⠀⠀⢀⣴⠾⠛⡁⠀⠀⢀⣈⡉⠙⠳⣤⡀⠀⠀⠀⠘⣆⠀⣇⡼⢋⠀⠀⢱
+//     ⠀⠘⣇⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⡴⢋⡣⠊⡩⠋⠀⠀⠀⠣⡉⠲⣄⠀⠙⢆⠀⠀⠀⣸⠀⢉⠀⢀⠿⠀⢸
+//     ⠀⠀⠸⡄⠀⠈⢳⣄⡇⠀⠀⢀⡞⠀⠈⠀⢀⣴⣾⣿⣿⣿⣿⣦⡀⠀⠀⠀⠈⢧⠀⠀⢳⣰⠁⠀⠀⠀⣠⠃
+//     ⠀⠀⠀⠘⢄⣀⣸⠃⠀⠀⠀⡸⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠈⣇⠀⠀⠙⢄⣀⠤⠚⠁⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢰⣿⣿⣿⡿⠛⠁⠀⠉⠛⢿⣿⣿⣿⣧⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⣸⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡀⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇⠹⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠁⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣤⣞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⣀⣠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠙⠲⢤⣀⣀⠀⢀⣀⣀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//       ''';
+      // });
+    }
+    if (response.body == 'null') {
+      // setState(() {
+      //   _isLoading = false;
+      // });
+      return [];
+    }
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    return loadedItems;
+    //} catch (error) {
+//     _error = '''
+// Something has gone terribly wrong. Please, please try again later.
+
+//     ⡴⠒⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠉⠳⡆⠀
+//     ⣇⠰⠉⢙⡄⠀⠀⣴⠖⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠁⠙⡆
+//     ⠘⡇⢠⠞⠉⠙⣾⠃⢀⡼⠀⠀⠀⠀⠀⠀⠀⢀⣼⡀⠄⢷⣄⣀⠀⠀⠀⠀⠀⠀⠀⠰⠒⠲⡄⠀⣏⣆⣀⡍
+//     ⠀⢠⡏⠀⡤⠒⠃⠀⡜⠀⠀⠀⠀⠀⢀⣴⠾⠛⡁⠀⠀⢀⣈⡉⠙⠳⣤⡀⠀⠀⠀⠘⣆⠀⣇⡼⢋⠀⠀⢱
+//     ⠀⠘⣇⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⡴⢋⡣⠊⡩⠋⠀⠀⠀⠣⡉⠲⣄⠀⠙⢆⠀⠀⠀⣸⠀⢉⠀⢀⠿⠀⢸
+//     ⠀⠀⠸⡄⠀⠈⢳⣄⡇⠀⠀⢀⡞⠀⠈⠀⢀⣴⣾⣿⣿⣿⣿⣦⡀⠀⠀⠀⠈⢧⠀⠀⢳⣰⠁⠀⠀⠀⣠⠃
+//     ⠀⠀⠀⠘⢄⣀⣸⠃⠀⠀⠀⡸⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠈⣇⠀⠀⠙⢄⣀⠤⠚⠁⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢰⣿⣿⣿⡿⠛⠁⠀⠉⠛⢿⣿⣿⣿⣧⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⣸⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡀⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇⠹⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠁⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣤⣞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⣀⣠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//     ⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠙⠲⢤⣀⣀⠀⢀⣀⣀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//       ''';
+    //}
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -23,46 +109,95 @@ class _HomeState extends State<Home> {
     if (newItem == null) {
       return;
     }
+
     setState(() {
       _groceryItems.add(newItem);
     });
   }
 
+  void removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    final url = Uri.https('shopping-list-7ec0f-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+
+    final response = await http.delete(url);
+    //checking if the communication with firebase went through if not we add back
+    if (response.statusCode >= 400) {
+      //Optional: show error meesage
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget content = const Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: 20,
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            'Let\'s go get some GROCERIES!!!!',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-            textAlign: TextAlign.center,
-          ),
-        ])
-      ],
-    );
+    // Widget content = const Center(
+    //   child: Text(
+    //     'Let\'s go get some GROCERIES!!!!',
+    //     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+    //     textAlign: TextAlign.center,
+    //   ),
+    // );
 
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => GroceryListWidget(
-          index: index,
-          groceryItems: _groceryItems,
-        ),
-      );
-    }
+    // if (_isLoading) {
+    //   content = const Center(
+    //     child: CircularProgressIndicator(),
+    //   );
+    // }
 
+    // if (_groceryItems.isNotEmpty) {
+    //   content = ListView.builder(
+    //     itemCount: _groceryItems.length,
+    //     itemBuilder: (context, index) => GroceryListWidget(
+    //       index: index,
+    //       groceryItems: _groceryItems,
+    //       item_remover: removeItem,
+    //     ),
+    //   );
+    // }
+    // if (_error != null) {
+    //   content = Center(
+    //     child: Text(_error!),
+    //   );
+    // }
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Groceries'),
-          actions: [
-            IconButton(onPressed: _addItem, icon: const Icon(Icons.add))
-          ],
-        ),
-        body: content);
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+      body: FutureBuilder(
+        future: _loadedItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+          if (snapshot.data!.isEmpty) {
+            const Center(
+              child: Text(
+                'Let\'s go get some GROCERIES!!!!',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) => GroceryListWidget(
+              index: index,
+              groceryItems: snapshot.data!,
+              item_remover: removeItem,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/widgets/dummy_item.dart
+++ lib/widgets/dummy_item.dart
@@ -3,17 +3,27 @@ import 'package:shopping_list_app/models/grocery.dart';
 
 class GroceryListWidget extends StatelessWidget {
   const GroceryListWidget(
-      {super.key, required this.index, required this.groceryItems});
+      {super.key,
+      required this.index,
+      required this.groceryItems,
+      required this.item_remover});
 
   final int index;
   final List<GroceryItem> groceryItems;
+  final void Function(GroceryItem item) item_remover;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(groceryItems[index].name),
-      leading: Icon(Icons.square, color: groceryItems[index].category.color),
-      trailing: Text(groceryItems[index].quantity.toString()),
+    return Dismissible(
+      key: ValueKey(groceryItems[index].id),
+      onDismissed: (direction) {
+        item_remover(groceryItems[index]);
+      },
+      child: ListTile(
+        title: Text(groceryItems[index].name),
+        leading: Icon(Icons.square, color: groceryItems[index].category.color),
+        trailing: Text(groceryItems[index].quantity.toString()),
+      ),
     );
   }
 }
